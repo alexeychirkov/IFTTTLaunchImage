d81ab6848cc1d6d5de62f0538908ec0b6e372691
Alexey Chirkov <alexey.chirkov@gmail.com>
2018-12-11 12:31:12 +0200

    "Xs Max" and "Xr" support

--- IFTTTLaunchImage/UIImage+IFTTTLaunchImage.m
+++ IFTTTLaunchImage/UIImage+IFTTTLaunchImage.m
@@ -21,8 +21,9 @@ NSString * const kIFTTTDefaultLaunchImageSetName = @"LaunchImage";
 
 + (instancetype)IFTTTLaunchImageNamed:(NSString *)name {
     NSMutableString *imageName = [NSMutableString stringWithString:name];
-    
-    CGFloat height = CGRectGetHeight([UIScreen mainScreen].bounds);
+    UIScreen *screen = [UIScreen mainScreen];
+    CGFloat height = CGRectGetHeight(screen.bounds);
+    CGSize screenSize = screen.currentMode.size;
     
     if ([[UIDevice currentDevice] IFTTTDeviceIsiPad]) {
         if ([[UIDevice currentDevice] IFTTTDeviceIsLandscape]) {
@@ -44,10 +45,20 @@ NSString * const kIFTTTDefaultLaunchImageSetName = @"LaunchImage";
         }
     } else if (height == 812.f) {
         [imageName appendString:@"-1100-Portrait-2436h"];
+    } else if (height == 896.f) {
+        if (screenSize.height == 2688) {
+            //Xs Max
+            [imageName appendString:@"-1200-Portrait-2688h"];
+        } else {
+            //Xr
+            [imageName appendString:@"-1200-Portrait-1792h"];
+        }
     } else {
         [imageName appendString:@"-700"];
     }
-        
+    //Xs max <UIScreen: 0x7fbd3c601eb0; bounds = {{0, 0}, {414, 896}}; mode = <UIScreenMode: 0x600002590440; size = 1242.000000 x 2688.000000>>
+    //xr     <UIScreen: 0x7fab9f4059a0; bounds = {{0, 0}, {414, 896}}; mode = <UIScreenMode: 0x6000013881a0; size = 828.000000 x 1792.000000>>
+    //Xs     <UIScreen: 0x7faf57c08960; bounds = {{0, 0}, {375, 812}}; mode = <UIScreenMode: 0x60000189df00; size = 1125.000000 x 2436.000000>>
     return [UIImage imageNamed:imageName];
 }
 
